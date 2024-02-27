785170caebdbb7f68fa38fb50903a3b4a8953210
Sudin <sudean.mdrr@gmail.com>
2024-02-26 22:53:34 -0600

    login page

---
+++ app/src/main/java/com/example/assignment_2/ui/login/Login.kt
@@ -0,0 +1,12 @@
+package com.example.assignment_2.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.assignment_2.R
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
