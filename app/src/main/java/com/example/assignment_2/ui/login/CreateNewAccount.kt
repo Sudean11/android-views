03c86b92b966f9fad182681d13017749c9a3a393
Sudin <sudean.mdrr@gmail.com>
2024-02-29 19:56:36 -0600

    week 4

---
+++ app/src/main/java/com/example/assignment_2/ui/login/CreateNewAccount.kt
@@ -0,0 +1,12 @@
+package com.example.assignment_2.ui.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.assignment_2.R
+
+class CreateNewAccount: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
