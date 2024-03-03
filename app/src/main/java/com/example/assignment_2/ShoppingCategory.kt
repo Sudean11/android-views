c1fc54382e41c0b0c52869c81493d6966bb6eaa3
Sudin <sudean.mdrr@gmail.com>
2024-03-03 13:09:26 -0600

    welcome tom

--- app/src/main/java/com/example/assignment_2/ShoppingCategory.kt
+++ app/src/main/java/com/example/assignment_2/ShoppingCategory.kt
@@ -1,10 +1,9 @@
-package com.example.assignment_2.ui.login
+package com.example.assignment_2
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.assignment_2.R
 
-class CreateNewAccount: AppCompatActivity() {
+class ShoppingCategory: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.create_new_account)
