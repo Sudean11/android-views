2b7ea5e0c5e947209c0d6480e1a8223caf2d0c67
Sudin <sudean.mdrr@gmail.com>
2024-03-03 13:46:41 -0600

    done

---
+++ app/src/main/java/com/example/assignment_2/Shopping.kt
@@ -0,0 +1,12 @@
+package com.example.assignment_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.assignment_2.R
+
+class Shopping : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
